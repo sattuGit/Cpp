fa7d96d5b1591a4142d2eaf623cbc48f62ba33b5
sattuGit <satendra.program2@gmail.com>
2021-05-08 17:58:26 +0000

    adding C style simple stack program with fix stack size

---
+++ DS/linie/stack_c.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_OVERFLOW  -91
+#define STACK_UNDERFLOW -92
+#define STACK_EMPTY     -90
+#define SIZE 10
+#define SUCCESS         1
+#define FAIL            0
+
+unsigned int stack[10];
+short head=-1;
+
+int isOverFLow(){
+    //printf("CALLED %d,%d",head,SIZE);
+	return ((head==(SIZE-1))?1:0);
+}
+
+int isUnderFlow(){
+    return ((head==-1)?1:0);
+}
+
+int push(int data){
+	if(isOverFLow()){
+    	return STACK_OVERFLOW;
+	}
+    stack[++head]=data;
+	return data;
+}
+
+int pop(){
+    if(isUnderFlow()){ return STACK_UNDERFLOW;}
+    int tmp = stack[head];
+    stack[head]=0;
+    --head;
+    return(tmp);
+}
+
+void printStack(){
+	for(int i=0;i<SIZE;++i)
+		printf("[%d]",stack[i]);
+	printf("\n");
+}
+
+void main(int argc, char** argv)
+{
+    unsigned int choice = 0;
+    while(1){
+        choice = 0;
+        printf("1 for Push \n");
+        printf("2 for Pop \n");
+        printf("3 for Exit...!!! \n");
+        scanf("%u",&choice);
+     
+	/*   TO-DO need to handle data type before reading
+	*/
+        if (choice==1)
+        {
+            if (isOverFLow()==STACK_OVERFLOW)
+            {
+                printf("Space Excepton..\n");
+            }else
+            {  
+                int data;
+                printf("Plese enter valid integer to push \n");
+                scanf("%d",&data);
+                if (push(data)== data)
+                {
+                    printf("Inserted...\n");
+                }else{
+                    printf("Unknown error ...\n");
+                }
+            }
+            
+            
+        }
+        else if (choice==2){
+            int tmp = pop();		
+            if (tmp==STACK_UNDERFLOW){
+                printf("NO DATA FOUND");
+            }else{
+				printf("Data Poped %d",tmp);;
+			}
+
+        }else if (choice == 3 )
+        {
+        	printf("exiting...\n");
+            break;
+        }else{
+        	printf("Invalid choice ...\n");
+        }
+		printStack();
+    }
+    return;
+}
