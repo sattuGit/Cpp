1b3d423c6d59d802f6457bf3b5ab2a18ebbf99a5
sattuGit <satendra.program2@gmail.com>
2021-05-25 19:50:25 +0000

    initilize garbage memory before use

--- CString/interviewtask.c
+++ CString/interviewtask.c
@@ -3,25 +3,26 @@
 void removeDuplicateChar(const char* inArgs,char* outArgs, short isCase)
 {
     if (inArgs==NULL) return ;
-    char pool[255];
+    int pool[256];
+    for(int i=0;i<256;++i)pool[i]=0;
     int index=0,
         targetindex=0,
         tmp=0;
     for(;inArgs[index]!='\0';++index){
         tmp = inArgs[index];
-        printf("\n************\n %c ",tmp); 
+        //printf("\n************\n %c ",tmp); 
         if( !isCase && (tmp>64 && tmp<(65+26)))
         {    
             tmp+=32;
-            printf(" CONVERT TO %c",tmp);
+            //printf(" CONVERT TO %c",tmp);
         }
         if (pool[tmp]!=1)
         {
-            printf(" put at %d",targetindex);
+            //printf(" put at %d",targetindex);
             outArgs[targetindex]=tmp;
             pool[tmp]=1;
             ++targetindex;
-        }else printf(" skipped ");
+        }//else printf(" skipped ");
     }
     putc('\n',stdout);
     outArgs[targetindex+1]='\0';
