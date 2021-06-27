01a9bb53a808670588db4c7e50c583a84477521c
sattuGit <36150574+sattuGit@users.noreply.github.com>
2021-06-27 13:27:48 +0530

    Adding node for dubbly l

--- DS/base/nodes.h
+++ DS/base/nodes.h
@@ -8,7 +8,7 @@
 #ifndef NODES_H_
 #define NODES_H_
 #include <iostream>
-using namespace std;
+
 struct Node{
 	public :
 		Node(int argData=0){
@@ -61,5 +61,27 @@ public:
 private:
 	void* address;	//non GNU data type to hold int address i.e. can hold address C++11 onward
 	LoopNode *nextNode;
+};
+
+/*Simple dubbly ll node, canbe use in singly as well (but lead memory waste)*/
+class DNode{
+    protected:
+        int _data;
+        Node *_next;
+        Node *_previous;
+    public:
+        Node(int data, Node *nxt=nullptr,Node *pre=nullptr){
+            _data =data;
+            _next= nxt;
+            _previous = pre;
+        }
+    
+        int getData()const{return _data;}
+        Node* getNext() const {return _next;}
+        Node* getPrevious() const {return _previous;}
+
+        int setData(int argData){ return _data=argData;}
+        bool setNext(Node *addr){_next =  addr;}
+        bool setPrevious(Node *addr){_previous =  addr;}        
 };
 #endif /* NODES_H_ */
