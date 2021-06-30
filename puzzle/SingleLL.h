0c981acee0d324654db657dfc999b72ac0706290
sattuGit <satendra.program2@gmail.com>
2021-06-30 16:11:54 +0000

    add operator overloading in linklist node for next node

--- puzzle/SingleLL.h
+++ puzzle/SingleLL.h
@@ -1,32 +1,6 @@
-#ifndef _NODE_
-#define _NODE_
-
-class node{
-    
-};
-
-class Node{
-    protected:
-        int _data;
-        Node *_next;
-        Node *_previous;
-    public:
-        Node(int data, Node *nxt=nullptr,Node *pre=nullptr){
-            _data =data;
-            _next= nxt;
-            _previous = pre;
-        }
-    
-        int getData()const{return _data;}
-        Node* getNext() const {return _next;}
-        Node* getPrevious() const {return _previous;}
-
-        int setData(int argData){ return _data=argData;}
-        bool setNext(Node *addr){_next =  addr;}
-        bool setPrevious(Node *addr){_previous =  addr;}
-        
-};
-
+#ifndef _PUZZ_SLL_
+#define _PUZZ_SLL_
+#include "../DS/base/nodes.h"
 class SingleLL{
     public:
         SingleLL(){
@@ -35,17 +9,17 @@ class SingleLL{
         
         void addNode(int data){
             if(_head==nullptr){
-                _head = new Node(data);
+                _head = new DNode(data);
                 return;
             } 
-            _head = new Node(data,_head);
+            _head = new DNode(data,_head);
         }
         
         bool isEmpty()const{return _head==nullptr?true:false;}
         int getHeadData()const {return _head->getData();}
         
         void displayList(){
-            Node *tmp = _head;
+            DNode *tmp = _head;
             while(tmp != nullptr){
                 std::cout<<"["<<tmp->getData()<<"]->";
                 tmp=tmp->getNext();
@@ -54,12 +28,13 @@ class SingleLL{
         }
         
         int returnMid(){
-            Node *tmp = _head,
+            DNode *tmp = _head,
                 *mid=_head;
             bool ticker= false;
             
             while(tmp!=nullptr){
-                tmp= tmp->getNext();
+                //tmp= tmp->getNext();
+                tmp = ++tmp;
                 if(ticker){
                     mid=mid->getNext();
                 }
@@ -69,7 +44,7 @@ class SingleLL{
             return mid!=nullptr?mid->getData():0;
         }
     private:
-        Node *_head;
+        DNode *_head;
 };
 
 #endif
---
+++ puzzle/testMidLinkList.hpp
@@ -0,0 +1,4 @@
+#ifndef _PUZZ_testMIDLL_
+#define _PUZZ_testMIDLL_
+bool testMidNodeLinkList();
+#endif
