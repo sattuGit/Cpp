697cf499718e864f0928b055ff8782b1808036d2
sattuGit <satendra.program2@gmail.com>
2021-07-09 19:46:33 +0000

    build bug fix

--- compiler/sum.h
+++ compiler/sum.h
@@ -1,8 +1,8 @@
 //#ifdef __cplusplus
 //extern "C" {
 //#endif
-int sum(int a, int b);
-float sum(float a, float b);
+int sumI(int a, int b);
+float sumF(float a, float b);
 
 //#ifdef __cplusplus
 //}//extern "C" 
