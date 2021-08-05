23ee33e4fd196c6ac1bedf12d698b0568fb43e3f
EC2 Default User <ec2-user@ip-172-31-84-37.ec2.internal>
2021-08-05 05:44:12 +0000

    draft pipe

---
+++ IPC/pipeSimplex.c
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+    
+    /*read write descriptor 0 for read, 1 for write */
+    int pipeDesc[2];
+    
+    int status =  pipe(pipeDesc);
+    if(status==-1){
+        std::cout << "pipe failed " << std::endl;
+        return EXIT_FAILURE;
+    }    
+    
+    char writeBuff[20],readBuff[20];
+
+    strcpy(writeBuff,"Hi test msg");
+    /*write in piple*/
+    write(pipeDesc[1],writeBuff,20*sizeof(char));
+    
+    /*Read from pipe*/
+    read(pipeDesc[0],readBuff,20*sizeof(char));
+    
+    std::cout << "Read msg _"<< readBuff <<"_"<< std::endl;
+    
+    read(pipeDesc[0],readBuff,20*sizeof(char));
+    
+    std::cout << "Read msg _"<< readBuff <<"_"<< std::endl;
+    
+}
